72efabc4fc5075b91462d148184328a2a781df06
nyakundigeorge <gee.nyakundi@gmail.com>
2020-05-10 07:53:36 +0300

    feat(error): added error handling

--- src/utils/errors/errors.go
+++ src/utils/errors/errors.go
@@ -1,1 +1,39 @@
 package errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+type RestErr struct {
+	Message string `json:"message"`
+	Status int `json:"status"`
+	Error string `json:"error"`
+}
+
+func NewError(msg string)  error{
+	return errors.New(msg)
+}
+func NewBadRequestError(msg string) *RestErr  {
+	return &RestErr{
+		Message: msg,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(msg string) *RestErr{
+	return &RestErr{
+		Message: msg,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError (msg string) *RestErr{
+	return &RestErr{
+		Message: msg,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+}
